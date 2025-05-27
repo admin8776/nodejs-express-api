f6cb4f44efe0d5d1344ed9956ab544df803e8e1d
admin8776 <162722984+admin8776@users.noreply.github.com>
2025-05-27 15:49:00 +0200

    Update and rename index.js to hidden-vpn.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('hello world programming')
-})
-app.listen(process.env.PORT || 3000)
